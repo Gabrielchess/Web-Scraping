84e16a457a6d7c09a5fc9335c8843edc866ed6bc
Gabriel Sanglard Senra <75097097+Gabrielchess@users.noreply.github.com>
2021-11-21 19:33:41 -0300

    Delete Regressão directory

--- Regressão/regressao_svr_casas.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Mar  3 10:38:11 2020
-
-@author: James Bond
-"""
-
--- Regressão/untitled0.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Mar 18 21:50:27 2020
-
-@author: Hacker James Bond
-"""
-
-Analize da regressao linear e multipla entre variaveis distintas
-
